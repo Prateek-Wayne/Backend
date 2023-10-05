d27904203a285d9db7b05e4f9306f6549151fc41
Sushannt <kumar.su5hant@yahoo.com>
2023-10-05 11:49:41 +0530

    fix: added environment variables and db-connection file

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import userRoute from './router/userRouter.js';
-import cookieParser from 'cookie-parser';
-import taskRouter from './router/taskRouter.js';
-import {authenticator} from './middleware/authenticator.js';
+import express from "express";
+import "dotenv/config";
+import connectToDB from "./db-connection.js";
+import userRoute from "./router/userRouter.js";
+import cookieParser from "cookie-parser";
+import taskRouter from "./router/taskRouter.js";
+import { authenticator } from "./middleware/authenticator.js";
 
-
-const uri = "mongodb+srv://lordprateekverma:rimFFlND5oxTl9NQ@cluster0.j2bcqvd.mongodb.net/TaskManagementTool?retryWrites=true&w=majority";
-mongoose.connect(uri).then(() => {
-    console.log("DB connected");
-}).catch((error) => {
-    console.log(error);
-});
+//connecting to database
+connectToDB(process.env.MONGO_URI);
 
 const app = express();
-const PORT = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cookieParser());//this is enables cookie data to be read
+app.use(cookieParser()); //this enables cookie data to be read
 
-app.use('/api/user/',userRoute);
-app.use('/api/task',authenticator,taskRouter);
-app.listen(PORT, () => {
-    console.log('Server started at PORT ', PORT);
-})
+app.use("/api/user/", userRoute);
+app.use("/api/task", authenticator, taskRouter);
+app.listen(port, () => {
+  console.log(`server started listening at http://localhost:${port}`);
+});
